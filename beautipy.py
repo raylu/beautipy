8bd90ec2792a0bbddc83431b4c8d5237c089125c
raylu <90059+raylu@users.noreply.github.com>
2023-03-21 00:16:26 -0700

    refactor into _format_line

--- beautipy.py
+++ beautipy.py
@@ -17,43 +17,52 @@ def beautify(f: typing.BinaryIO) -> typing.Iterable[str]:
 	assert encoding.type == token.ENCODING
 
 	indentation = 0
-	stack = Stack()
-	line = ''
+	line_tokens: list[tokenize.TokenInfo] = []
 	for tok in tokens:
 		print(token.tok_name[tok.exact_type], tok, file=sys.stderr)
 		if tok.type == token.NEWLINE:
-			yield '\t' * indentation + line
-			line = ''
+			yield '\t' * indentation + _format_line(line_tokens)
+			line_tokens.clear()
 		elif tok.type == token.INDENT:
 			indentation += 1
 		elif tok.type == token.DEDENT:
 			indentation -= 1
 		else:
-			# prefix
-			if tok.type == token.OP:
-				if tok.exact_type == token.EQUAL and stack.top() != token.LPAR:
-					line += ' '
+			line_tokens.append(tok)
 
-			line += tok.string
+	assert tok.type == token.ENDMARKER
 
-			# suffix
-			if tok.type == token.NAME:
-				if keyword.iskeyword(tok.string) and tok.string not in ('True', 'False', 'None'):
-					line += ' '
-			elif tok.type == token.OP:
-				if tok.exact_type == token.COMMA:
-					line += ' '
-				elif tok.exact_type == token.EQUAL and stack.top() != token.LPAR:
-					line += ' '
-				elif tok.exact_type == token.COLON and stack.top() == token.LBRACE:
-					line += ' '
+def _format_line(line_tokens: list[tokenize.TokenInfo]) -> str:
+	line = ''
+	stack = Stack()
+	for tok in line_tokens:
+		# prefix
+		if tok.type == token.OP:
+			if tok.exact_type == token.EQUAL and stack.top() != token.LPAR:
+				line += ' '
 
-			# modify stack
-			if tok.type == token.OP:
-				if tok.exact_type in (token.LPAR, token.LSQB, token.LBRACE):
-					stack.push(tok.exact_type)
-				elif tok.exact_type in (token.RPAR, token.RSQB, token.RBRACE):
-					stack.pop(tok.exact_type)
+		line += tok.string
+
+		# suffix
+		if tok.type == token.NAME:
+			if keyword.iskeyword(tok.string) and tok.string not in ('True', 'False', 'None'):
+				line += ' '
+		elif tok.type == token.OP:
+			if tok.exact_type == token.COMMA:
+				line += ' '
+			elif tok.exact_type == token.EQUAL and stack.top() != token.LPAR:
+				line += ' '
+			elif tok.exact_type == token.COLON and stack.top() == token.LBRACE:
+				line += ' '
+
+		# modify stack
+		if tok.type == token.OP:
+			if tok.exact_type in (token.LPAR, token.LSQB, token.LBRACE):
+				stack.push(tok.exact_type)
+			elif tok.exact_type in (token.RPAR, token.RSQB, token.RBRACE):
+				stack.pop(tok.exact_type)
+
+	return line
 
 class Stack:
 	def __init__(self):
