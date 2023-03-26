9728857ae0d428c5e75b91af408bf8fb6cf6b2cc
raylu <90059+raylu@users.noreply.github.com>
2023-03-26 00:33:24 -0700

    use metaclasses to generate separate tests
    this makes it appear nicely in the vscode testing panel
    (pytest doesn't support the load_tests protocol)
    
    also add a vscode config for pytest

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,18 +1,25 @@
 import pathlib
+import typing
 import unittest
 
 import beautipy
 
-class Test(unittest.TestCase):
-	def tests(self):
-		for case in pathlib.Path(__file__).parent.glob('case*'):
-			if str(case).endswith('_expected'):
-				continue
-			with self.subTest(case):
-				self.run_testcase(case)
-	
-	def run_testcase(self, case: pathlib.Path):
+def _make_test(case: pathlib.Path) -> typing.Callable:
+	def test(self: 'Test') -> None:
 		with case.open('rb') as f:
 			result = '\n'.join(beautipy.beautify(f, None)) + '\n'
 		with case.with_name(case.name + '_expected').open('r') as f:
 			assert result == f.read()
+
+	return test
+
+class Meta(type):
+	def __new__(cls, name, bases, attrs):
+		for case in pathlib.Path(__file__).parent.glob('case*'):
+			if str(case).endswith('_expected'):
+				continue
+			attrs['test_' + case.name] = _make_test(case)
+		return type.__new__(cls, name, bases, attrs)
+
+class Test(unittest.TestCase, metaclass=Meta):
+	pass
